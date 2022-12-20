bec906e09a0b1114aff71fdd19e3e0e51a6c7b65
Mohamed Asir <asir.abm@gmail.com>
2022-12-20 13:30:27 +0530

    end

--- src/App.js
+++ src/App.js
@@ -4,106 +4,134 @@ import DigitButton from './componets/DigitButton';
 import OpButton from './componets/OpButton';
 import './styles.css'
 
-export const ACTION={
-   ADD_DIGIT:'adding-digit',
-   ADD_OPER:'adding-operation',
-   CLEAR:'clear',
-   EVELUATE:'eveluate'
+
+const INTEGER_FORMATTER = new Intl.NumberFormat('en-us', {
+  maximumFractionDigits: 0
+})
+function formart_op(op) {
+  if (op === '') {
+    return
+  }
+  if (op.includes('.')) {
+    const [inte, deci] = op.split('.')
+    return `${INTEGER_FORMATTER.format(inte)}.${deci}`
+  }
+  return INTEGER_FORMATTER.format(op)
+
 }
 
-const initialState={
-  cOp:'',
-  pOp:'',
-  operation:'',
-  overwrite:false
+
+export const ACTION = {
+  ADD_DIGIT: 'adding-digit',
+  ADD_OPER: 'adding-operation',
+  CLEAR: 'clear',
+  EVELUATE: 'eveluate',
+  DELETE: 'delete-digit'
 }
 
-const reducer=(state,{type,payload})=>{
-  switch(type){
+const initialState = {
+  cOp: '',
+  pOp: '',
+  operation: '',
+  overwrite: false
+}
+
+const reducer = (state, { type, payload }) => {
+  switch (type) {
     case ACTION.ADD_DIGIT:
-      if(state.overwrite){
-        return {...state,cOp:payload.digit,overwrite:false}
+      if (state.overwrite) {
+        return { ...state, cOp: payload.digit, overwrite: false }
       }
-      if( payload.digit === '0' &&  state.cOp === '0'){
-      return state
-      }
-      console.log(state.cOp)
-      if(payload.digit === '.' && state.cOp.includes('.')){
+      if (payload.digit === '0' && state.cOp === '0') {
         return state
       }
-      return {...state,cOp:`${state.cOp || '' }${payload.digit}`}
+     // console.log(state.cOp)
+      if (payload.digit === '.' && state.cOp.includes('.')) {
+        return state
+      }
+      return { ...state, cOp: `${state.cOp || ''}${payload.digit}` }
 
-      case ACTION.ADD_OPER:
-        // console.log('Asir')
-        if(state.cOp === '' && state.pOp === ''){
-          return state
-        }
+    case ACTION.ADD_OPER:
+      // console.log('Asir')
+      if (state.cOp === '' && state.pOp === '') {
+        return state
+      }
 
-        if(state.pOp===''){
-          return {
-            ...state,
-            pOp:state.cOp,
-            operation:payload.op,
-            cOp:''
-          }
-        }
-        if(state.cOp===''){
-          return{
-            ...state,
-            operation:payload.op
-          }
-
-        }
+      if (state.pOp === '') {
         return {
           ...state,
-          pOp:eveluate(state),
-          operation:payload.op,
-          cOp:''
+          pOp: state.cOp,
+          operation: payload.op,
+          cOp: ''
         }
+      }
+      if (state.cOp === '') {
+        return {
+          ...state,
+          operation: payload.op
+        }
+
+      }
+      return {
+        ...state,
+        pOp: eveluate(state),
+        operation: payload.op,
+        cOp: ''
+      }
     case ACTION.CLEAR:
-      return {cOp:'',pOp:'',operation:''}
-     
+      return { cOp: '', pOp: '', operation: '' }
+
     case ACTION.EVELUATE:
-      console.log('From eveluate')
-      if(state.cOp === ''|| state.pOp=== '' || state.operation ==''){
+     // console.log('From eveluate')
+      if (state.cOp === '' || state.pOp === '' || state.operation === '') {
         return state
       }
       return {
         ...state,
-        
-        operation:'',
-        pOp:'',
-        cOp:eveluate(state),
-        overwrite:true
+
+        operation: '',
+        pOp: '',
+        cOp: eveluate(state),
+        overwrite: true
       }
-    default:
+    case ACTION.DELETE:
+      if (state.cOp === '') {
         return state
-   }
-  
+      }
+      if (state.cOp.length === 1) {
+        return { ...state, cOp: '' }
+      }
+      return { ...state, cOp: state.cOp.slice(0, -1) }
+    default:
+      return state
+  }
+
 
 }
 
-const eveluate=({cOp,pOp,operation})=>{
+const eveluate = ({ cOp, pOp, operation }) => {
 
-  const prev=parseFloat(pOp)
-  const current=parseFloat(cOp)
+  const prev = parseFloat(pOp)
+  const current = parseFloat(cOp)
 
-  if(isNaN(prev)||isNaN(current)) return ''
+  if (isNaN(prev) || isNaN(current)) return ''
 
-  let results=''
+  let results = ''
   switch (operation) {
     case '+':
-      results=prev+current
+      results = prev + current
       break;
-      case '-':
-        results=prev-current
-        break;
-        case '×':
-          results=current*prev
-          break;  
-          case '÷':
-            results=prev/current
-            break;
+    case '-':
+      results = prev - current
+      break;
+    case '×':
+      results = current * prev
+      break;
+    case '÷':
+      results = prev / current
+      break;
+    default:
+      break;
 
   }
   return results.toString()
@@ -113,34 +141,34 @@ const eveluate=({cOp,pOp,operation})=>{
 }
 
 function App() {
- const [{cOp,pOp,operation},dispatch]= useReducer(reducer,initialState)
+  const [{ cOp, pOp, operation }, dispatch] = useReducer(reducer, initialState)
   return (
     <div className='calculator-grid'>
-       <div className='output-display'>
-         <div className='p-value'>{pOp} {operation}</div>
-         <div className='c-value'>{cOp}</div>
-       </div>
-       
-       <button onClick={()=>dispatch({type:ACTION.CLEAR})} className='span-two'>AC</button>
-       <button>DEL</button>
-       <OpButton dispatch={dispatch} op='÷'/>
-       <DigitButton dispatch={dispatch} digit='1'/>
-       <DigitButton dispatch={dispatch} digit='2'/>
-       <DigitButton dispatch={dispatch} digit='3'/>
-       <OpButton dispatch={dispatch} op='×'/>
-       <DigitButton dispatch={dispatch} digit='4'/>
-       <DigitButton dispatch={dispatch} digit='5'/>
-       <DigitButton dispatch={dispatch} digit='6'/>
-       <OpButton dispatch={dispatch} op='+'/>
-       <DigitButton dispatch={dispatch} digit='7'/>
-       <DigitButton dispatch={dispatch} digit='8'/>
-       <DigitButton dispatch={dispatch} digit='9'/>
-       <OpButton dispatch={dispatch} op='-'/>
-       <DigitButton dispatch={dispatch} digit='.'/>
-       <DigitButton dispatch={dispatch} digit='0'/>
-       <button onClick={()=>dispatch({type:ACTION.EVELUATE})} className='span-two'>=</button>
-    
-       
+      <div className='output-display'>
+        <div className='p-value'>{formart_op(pOp)} {operation}</div>
+        <div className='c-value'>{formart_op(cOp)}</div>
+      </div>
+
+      <button onClick={() => dispatch({ type: ACTION.CLEAR })} className='span-two'>AC</button>
+      <button onClick={() => dispatch({ type: ACTION.DELETE })}>DEL</button>
+      <OpButton dispatch={dispatch} op='÷' />
+      <DigitButton dispatch={dispatch} digit='1' />
+      <DigitButton dispatch={dispatch} digit='2' />
+      <DigitButton dispatch={dispatch} digit='3' />
+      <OpButton dispatch={dispatch} op='×' />
+      <DigitButton dispatch={dispatch} digit='4' />
+      <DigitButton dispatch={dispatch} digit='5' />
+      <DigitButton dispatch={dispatch} digit='6' />
+      <OpButton dispatch={dispatch} op='+' />
+      <DigitButton dispatch={dispatch} digit='7' />
+      <DigitButton dispatch={dispatch} digit='8' />
+      <DigitButton dispatch={dispatch} digit='9' />
+      <OpButton dispatch={dispatch} op='-' />
+      <DigitButton dispatch={dispatch} digit='.' />
+      <DigitButton dispatch={dispatch} digit='0' />
+      <button onClick={() => dispatch({ type: ACTION.EVELUATE })} className='span-two'>=</button>
+
+
     </div>
   );
 }
